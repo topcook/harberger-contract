5a1fc7be20ea5ce9d77d8cdb2fa3987828f86ebd
topcook <codecooker1996@gmail.com>
2022-03-19 09:35:18 -0400

    I added contract file and deploy.js and arguments.js file.

---
+++ arguments.js
@@ -0,0 +1,3 @@
+module.exports = [
+  '0x09AE949950905cDd9b07EF7ba866bBa9d31Dd0FB'
+  ];
---
+++ contracts/HarbergerTaxed.sol
@@ -0,0 +1,265 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+/**
+ * @title HarbergerTaxed_v1
+ */
+contract HarbergerTaxed_v5 is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    struct HarbergerInfo {
+        // wallet address of owner
+        address owner;
+        // duration of the ownership in seconds
+        uint32 ownershipPeriod;
+        // timestamp when ownership started
+        uint256 startTime;
+        // timestamp when ownership ended
+        uint256 endTime;
+        // value of Harberger Hike
+        uint16 harbergerHike;
+        // value of Harberger Tax
+        uint16 harbergerTax;
+        // value of Initial Price
+        uint256 initialPrice;
+        // value of final Price
+        uint256 finalPrice;
+        // value of string will be sold
+        string valueOfString;
+    }
+
+    // address of the ERC20 token
+    IERC20 private immutable _token;
+
+    address public issuer;
+
+    uint8 decimals = 18;
+
+    // value for minutes in a day
+    uint32 private SECONDS_IN_DAY;
+
+    // instance of Harberger
+    HarbergerInfo public harbergerInfo;
+
+    // event TokenVested(address indexed accout, uint256 amount);
+
+    event OwnershipPeriodChangedEvent(uint32 ownershipPeriod);
+
+    event OwnershipChangedEvent(address indexed account);
+
+    event DelayEndTimeOfOwnershipEvent(address indexed account);
+
+    modifier notIssuer() {
+        require(harbergerInfo.owner != issuer);
+        _;
+    }
+
+    modifier onlyOwnerOfHarberger() {
+        require(msg.sender == harbergerInfo.owner);
+        _;
+    }
+
+    /**
+     * @dev Creates a vesting contract.
+     * @param token_ address of the ERC20 token contract
+     */
+    constructor(address token_) {
+
+        _token = IERC20(token_);
+
+        issuer = msg.sender;
+
+        SECONDS_IN_DAY = 30; // 30s for testnet,  60 * 60 * 24s for mainnet;
+
+        uint32 ownershipPeriod = SECONDS_IN_DAY * 7; // SECONDS_IN_DAY * 7 for mainnet , 90 for testnet
+        // timestamp when ownership started
+        uint256 startTime = block.timestamp;
+        // timestamp when ownership ended
+        // uint32 endTime = block.timestamp + ownershipPeriod;
+        uint256 endTime = block.timestamp + 60 * 60 * 24 * 365 * 10;
+        // value of Harberger Hike
+        uint16 harbergerHike = 20;
+        // value of Harberger Tx
+        uint16 harbergerTax = 10;
+        // value of Initial Price
+        uint256 initialPrice = 100 * (10 ** decimals);
+        // value of string
+        string memory valueOfString = "first string";
+
+        // create Harberger
+        createHarbergerInfo(
+            ownershipPeriod,
+            startTime,
+            endTime,
+            harbergerHike,
+            harbergerTax,
+            initialPrice,
+            valueOfString
+        );
+    }
+
+    /**
+     * @notice Creates a new vesting schedule for an account.
+     * @param _ownershipPeriod duration of the ownership in seconds
+     * @param _startTime timestamp when ownership started
+     * @param _endTime timestamp when ownership ended
+     * @param _harbergerHike value of Harberger Hike
+     * @param _harbergerTax value of Harberger Tax
+     * @param _initialPrice value of Initial Price
+     * @param _valueOfString value of string will be sold
+     */
+    function createHarbergerInfo(
+        uint32 _ownershipPeriod,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint16 _harbergerHike,
+        uint16 _harbergerTax,
+        uint256 _initialPrice,
+        string memory _valueOfString
+    ) internal onlyOwner {
+        require(_ownershipPeriod > 0, "Ownership period must be > 0");
+        require(_startTime >= block.timestamp, "Ownership start time must be after the present");
+        require(_initialPrice > 0, "Initial Price must be > 0");
+
+        harbergerInfo = HarbergerInfo(
+            // address(this),
+            msg.sender,
+            _ownershipPeriod,
+            _startTime,
+            _endTime,
+            _harbergerHike,
+            _harbergerTax,
+            _initialPrice,
+            // _initialPrice,
+            _initialPrice,
+            _valueOfString
+        );
+    }
+
+    function getOwnershipPeriod() public view returns (uint32) {
+        return harbergerInfo.ownershipPeriod;
+    }
+
+    function getHarbergerHike() public view returns (uint32) {
+        return harbergerInfo.harbergerHike;
+    }
+
+    function getHarbergerTax() public view returns (uint32) {
+        return harbergerInfo.harbergerTax;
+    }
+
+    function getInitialPrice() public view returns (uint256) {
+        return harbergerInfo.initialPrice;
+    }
+
+    function getCurrentPrice() public view returns (uint256) {
+        return harbergerInfo.finalPrice;
+    }
+
+    function getValueOfString() public view returns (string memory) {
+        return harbergerInfo.valueOfString;
+    }
+
+    /**
+     * @notice Transfer ownership of string to other wallet
+     * @param _amount amount of tokens to pay for ownership
+     */
+    function TransferOwnershipOfHarberger(uint256 _amount) public {
+        address owner = harbergerInfo.owner;
+        uint256 endTime = harbergerInfo.endTime;
+
+        if (owner == issuer) {
+            //first sale
+            TransferOwnershipOfHarbergerAtFirst(_amount);
+        } else {
+            if (endTime >block.timestamp) {
+                //seconde sale
+                TransferOwnershipOfHarbergerAtSecond(_amount);
+            } else {
+                harbergerInfo.owner = issuer;
+                harbergerInfo.finalPrice = harbergerInfo.initialPrice;
+
+                //first sale
+                TransferOwnershipOfHarbergerAtFirst(_amount);               
+            }
+        }
+    }
+
+
+    /**
+     * @notice Transfer ownership of string to other wallet at first
+     * @param _amount amount of tokens to pay for ownership at first
+     */
+    function TransferOwnershipOfHarbergerAtFirst(uint256 _amount) private {
+        uint256 initialPrice = harbergerInfo.initialPrice;
+        uint256 harbergerTax = harbergerInfo.harbergerTax;
+        require(_amount >= initialPrice, "Token amount is not enough");
+
+        uint256 finalPrice = _amount + _amount * harbergerTax / 100;
+
+        _token.transferFrom(msg.sender, issuer, finalPrice);
+        harbergerInfo.owner = msg.sender;
+        harbergerInfo.startTime = block.timestamp;
+        harbergerInfo.endTime = block.timestamp + harbergerInfo.ownershipPeriod;
+        harbergerInfo.finalPrice = finalPrice;
+
+        emit OwnershipChangedEvent(msg.sender);
+    }
+
+    /**
+     * @notice Delay endTime of ownership
+     * @param _amount amount of tokens to pay for delaying the endTime of ownership     
+     */
+    function DelayEndTimeOfOwnership(uint256 _amount) public notIssuer onlyOwnerOfHarberger {
+        uint256 endTime = harbergerInfo.endTime;
+        uint256 harbergerTax = harbergerInfo.harbergerTax;
+        uint256 initialPrice = harbergerInfo.initialPrice;
+        uint256 ownershipPeriod = harbergerInfo.ownershipPeriod;
+        uint256 currentTime = block.timestamp;      
+
+        require(_amount >= initialPrice * harbergerTax / 100, "Token amount is not enough");
+        require(endTime - currentTime < ownershipPeriod, "You have already delayed end time of ownership");
+        _token.transferFrom(msg.sender, issuer, _amount);
+        harbergerInfo.endTime = endTime + ownershipPeriod;
+
+        emit DelayEndTimeOfOwnershipEvent(msg.sender);
+    }
+
+    /**
+     * @notice Transfer ownership of string to other wallet at second phase
+     * @param _amount amount of tokens to pay for ownership at second phase
+     */
+    function TransferOwnershipOfHarbergerAtSecond(uint256 _amount) private {
+        uint256 finalPrice = harbergerInfo.finalPrice;
+        uint16 harbergerTax = harbergerInfo.harbergerTax;
+        uint16 harbergerHike = harbergerInfo.harbergerHike;
+        uint256 ownershipPeriod = harbergerInfo.ownershipPeriod;
+        address owner = harbergerInfo.owner;
+
+        require(_amount >= finalPrice + finalPrice * harbergerHike / 100, "Token amount is not enough");
+        _token.transferFrom(msg.sender, issuer, _amount * harbergerTax / 100);
+        _token.transferFrom(msg.sender, owner, _amount + _amount * harbergerHike / 100);
+
+        harbergerInfo.owner = msg.sender;
+        harbergerInfo.startTime = block.timestamp;
+        harbergerInfo.endTime = block.timestamp + ownershipPeriod;
+        harbergerInfo.finalPrice = _amount + _amount * harbergerHike / 100 + _amount * harbergerTax / 100;
+
+        emit OwnershipChangedEvent(msg.sender);
+    }
+
+    /**
+     * @notice Set ownership time
+     * @param _days days of ownership
+     */
+    function setOwnershipPeriod(uint32 _days) public {
+        uint32 ownershipPeriod = _days * SECONDS_IN_DAY;
+        harbergerInfo.ownershipPeriod = ownershipPeriod;
+        emit OwnershipPeriodChangedEvent(ownershipPeriod);
+    }
+}
